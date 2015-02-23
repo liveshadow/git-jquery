e0800b66d25430c837983d8649fc06921678b97f
Olivia <olivia@Mesa.local>
2015-02-22 23:48:13 -0700

    build

--- js/my/repos.js
+++ js/my/repos.js
@@ -2,6 +2,24 @@ function myRepos(){
 
     // display my repos in #list
     // e.g., https://api.github.com/users/doubleshow/repos
+    console.log('listing repos')    
+    
+    $.get("https://api.github.com/users/liveshadow/repos", github, function(data) {
 
-    alert('todo')
+        var repos = data
+        
+        $.get("/git-jquery/templates/repoList.jade", function(template) {
+
+            // render the template
+            var html = jade.render(template, {items: repos})            
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html)
+
+            // load the first repo to view
+            repoView(repos[0].full_name)
+
+        })
+
+    })
 }
